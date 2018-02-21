70ccdbc3a557335c786373afffc72d2df641db01
Max Reno <mar257@pitt.edu>
2018-02-21 13:34:43 -0500

    hmm

--- project1/sampledriver.c
+++ project1/sampledriver.c
@@ -12,13 +12,9 @@ printf(1, "init_graphics: %d", init_graphics());
 printf(1, "clear_screen %d", clear_screen());
 // for(int i=0; i<16; i++)
 //   draw_pixel(320,320+i,i);
-
-printf(1, "blit: %d", blit());
-sleep(100);
-clear_screen();
 blit();
 
-// draw_line(100,200,500,200, 14);
+draw_line(100,200,500,200, 14);
 draw_line(500,400,100,400, 13);
 draw_line(600,200,600,500, 13);
 blit();
