ab029751eba501b9a1dbe484801acddf1cf6b30e
Max Reno <mar257@pitt.edu>
2018-02-21 12:01:11 -0500

    more tests

--- project1/sampledriver.c
+++ project1/sampledriver.c
@@ -10,16 +10,17 @@ printf(1, "init_graphics: %d", init_graphics());
 // printf(1, "getkey: %d", getkey());
 
 printf(1, "clear_screen %d", clear_screen());
+for(int i=0; i<16; i++)
+  draw_pixel(320,320+i,i);
 
-printf(1, "draw_pixel: %d", draw_pixel(320,320,1));
-printf(1, "draw_pixel: %d", draw_pixel(320,320,2));
-printf(1, "draw_pixel: %d", draw_pixel(320,320,3));
-printf(1, "draw_pixel: %d", draw_pixel(320,320,4));
+printf(1, "blit: %d", blit());
 sleep(100);
 sleep(100);
+sleep(100);
+sleep(100);
+
 
 printf(1, "draw_line: %d", draw_line(1,2,3,4,5));
-printf(1, "blit: %d", blit());
 printf(1, "Test");
 
 
