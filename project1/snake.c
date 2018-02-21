74bc857d53b47ec79f28511a965ef7c5f0c91599
Max Reno <mar257@pitt.edu>
2018-02-21 17:18:55 -0500

    first snake test

---
+++ project1/snake.c
@@ -0,0 +1,29 @@
+#include "types.h"
+#include "user.h"
+#include "syscall.h"
+
+#define start 320
+int main(void)
+{
+  draw_pixel(320,320,16);
+  while(1){
+    int key = getkey();
+    if(key!=-1){
+      // printf(1, "%d", key);
+
+      if(key==w){
+        printf(1, "w");
+      } else if(key==a){
+        printf(1, "a");
+
+      } else if(key==s){
+        printf(1, "s");
+
+      } else if(key==d){
+        printf(1, "d");
+
+      }
+    }
+    sleep(25);
+  }
+}
