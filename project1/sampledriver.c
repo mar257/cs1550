ef66c81d7203987a3297d78cf2b422a2c8643618
Max Reno <mar257@pitt.edu>
2018-02-21 13:32:31 -0500

    abs fix maybe

--- project1/sampledriver.c
+++ project1/sampledriver.c
@@ -10,24 +10,22 @@ printf(1, "init_graphics: %d", init_graphics());
 // printf(1, "getkey: %d", getkey());
 
 printf(1, "clear_screen %d", clear_screen());
-for(int i=0; i<300; i++)
-  draw_pixel(320,0+i,i%16);
+// for(int i=0; i<16; i++)
+//   draw_pixel(320,320+i,i);
 
 printf(1, "blit: %d", blit());
-sleep(500);
+sleep(100);
 clear_screen();
 blit();
 
-draw_line(100,200,500,200, 2);
-draw_line(200,200,200,500, 10);
-
-// draw_line(500,400,100,400, 13);
-// draw_line(600,200,600,500, 13);
+// draw_line(100,200,500,200, 14);
+draw_line(500,400,100,400, 13);
+draw_line(600,200,600,500, 13);
 blit();
 sleep(500);
 
 // printf(1, "draw_line: %d", draw_line(1,2,3,4,5));
-printf(1, "Test");
+// printf(1, "Test");
 
 
 exit();
