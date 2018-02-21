a1d10547c911176ac1b6a7ea68fbae41c0b9b542
Max Reno <mar257@pitt.edu>
2018-02-21 11:49:35 -0500

    add sleep to see what happens

--- project1/sampledriver.c
+++ project1/sampledriver.c
@@ -8,11 +8,15 @@ int main(void)
 printf(1, "init_graphics: %d", init_graphics());
 printf(1, "exit_graphics: %d", exit_graphics());
 printf(1, "getkey: %d", getkey());
+
 printf(1, "clear_screen %d", clear_screen());
+
 printf(1, "draw_pixel: %d", draw_pixel(320,320,1));
 printf(1, "draw_pixel: %d", draw_pixel(320,320,2));
 printf(1, "draw_pixel: %d", draw_pixel(320,320,3));
 printf(1, "draw_pixel: %d", draw_pixel(320,320,4));
+sleep(100);
+sleep(100);
 
 printf(1, "draw_line: %d", draw_line(1,2,3,4,5));
 printf(1, "blit: %d", blit());
