bdbf3da2b25a117b08577c8e041bba737556df85
Max Reno <mar257@pitt.edu>
2018-03-16 19:20:42 -0400

    ahhh

--- project2/proj2.c
+++ project2/proj2.c
@@ -2,9 +2,11 @@
 #include "user.h"
 #include "pstat.h"
 
-// Print out pstat object basically
 int main() {
-  printf(1, "Index\t Inuse\t Tickets\t PID\t Ticks \n");
+  struct pstat pst;
+  int i;
+
+  printf(1, "Index \tInuse \tTickets \tPID \tTicks \n");
   for (i=0; i<NPROC; i++) {
     printf(1, "%d\t%d\t%d\t%d\t%d\n", i,
     pst.inuse[i],
