7958de89c2d24004be4880a82ce43f36eae0b39e
Max Reno <mar257@pitt.edu>
2018-03-16 19:26:07 -0400

    final

--- project2/proj2.c
+++ project2/proj2.c
@@ -6,13 +6,13 @@ int main() {
   struct pstat pst;
   int i;
 
-  printf(1, "Index \tInuse \tTickets \tPID \tTicks \n");
+  printf(1, "# \tinuse \tntix \tticks \tpid \n");
   for (i=0; i<NPROC; i++) {
     printf(1, "%d\t%d\t%d\t%d\t%d\n", i,
     pst.inuse[i],
     pst.tickets[i],
-    pst.pid[i],
     pst.ticks[i]
+    pst.pid[i],
   );
 }
 exit();
