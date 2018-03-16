82dd4976cfcb324426134640b0ba5952e672211a
Max Reno <mar257@pitt.edu>
2018-03-16 18:53:41 -0400

    try again

--- project2/proj2.c
+++ project2/proj2.c
@@ -84,7 +84,7 @@ int main() {
                         break;
                 }
             }
-            printf(1, "%d,\t%d,\t%d\n", p1, p2+rand()%50, p3+rand()%100);
+            printf(1, "%d,\t%d,\t%d\n", p1, p2+rand()%40, p3+rand()%200);
         }
         exit();
     }
